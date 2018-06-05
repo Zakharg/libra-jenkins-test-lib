d9151b766d62b551433603f9315bb4e1f4d701f9
George Zakharov <george.zakharov@libra.tech>
2018-06-05 17:00:31 -0400

    Created a new project

---
+++ src/main/java/tech/libra/jenkins/test/lib/DumbCalculator.java
@@ -0,0 +1,9 @@
+package tech.libra.jenkins.test.lib;
+
+public interface DumbCalculator {
+	
+	int add(int first, int second);
+	
+	int sub(int first, int second);
+
+}
---
+++ src/main/java/tech/libra/jenkins/test/lib/DumbCalculatorImpl.java
@@ -0,0 +1,13 @@
+package tech.libra.jenkins.test.lib;
+
+public class DumbCalculatorImpl {
+	
+	public int add(int first, int second) {
+		return first - second;
+	}
+	
+	public int sub(int first, int second) {
+		return first + second;
+	}
+
+}
